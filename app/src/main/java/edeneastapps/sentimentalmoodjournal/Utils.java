1f259c05910b4c9e6ea3d1f089fb86803fc23a9c
Brian Roper <appdevbri@gmail.com>
2018-11-16 01:40:33 -0500

    fixed color calculations

--- app/src/main/java/edeneastapps/sentimentalmoodjournal/Utils.java
+++ app/src/main/java/edeneastapps/sentimentalmoodjournal/Utils.java
@@ -2,31 +2,31 @@ package edeneastapps.sentimentalmoodjournal;
 
 public class Utils {
     public static int returnSentimentRangeColor(float score){
-        if (score >= .80f){
+        if (score >= .08f){
             return R.color.blue500;
         }
-        else if(score >= .60f && score < .80f){
+        else if(score >= .06f && score < .08f){
             return R.color.lightblue500;
         }
-        else if(score >= .40f && score < .60f){
+        else if(score >= .04f && score < .06f){
             return R.color.cyan500;
         }
-        else if(score >= .20f && score < .40f){
+        else if(score >= .02f && score < .04f){
             return R.color.teal500;
         }
-        else if(score >= 0f && score < .20f){
+        else if(score >= 0f && score < .02f){
             return R.color.green500;
         }
-        else if(score >= (-.20f) && score < 0f){
+        else if(score >= (-.02f) && score < 0f){
             return R.color.lightgreen500;
         }
-        else if(score >= (-.40f) && score < (-.20f)){
+        else if(score >= (-.04f) && score < (-.02f)){
             return R.color.lime500;
         }
-        else if(score >= (-.60f) && score < (-40f)){
+        else if(score >= (-.06f) && score < (-04f)){
             return R.color.yellow400;
         }
-        else if(score >= (-.80f) && score < (-.60f)){
+        else if(score >= (-.08f) && score < (-.06f)){
             return R.color.amber500;
         }
         else{
